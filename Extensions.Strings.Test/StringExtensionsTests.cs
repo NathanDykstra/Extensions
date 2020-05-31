86633fb6711c3d75f4ae66d6a3a2e88e0962e902
Nathan Dykstra <NathanDykstra@users.noreply.github.com>
2020-05-30 20:06:49 -0500

    add tocamelcase, topascalcase, and toalphanumeric along with unit tests

--- Extensions.Strings.Test/StringExtensionsTests.cs
+++ Extensions.Strings.Test/StringExtensionsTests.cs
@@ -16,5 +16,38 @@ namespace Extensions.Strings.Test
             actual = s.ToNumeric();
             actual.Should().Be("123");
         }
+
+        [TestCase("camelCase", "camelCase")]
+        [TestCase("innerHTML", "innerHTML")]
+        [TestCase("String Extensions", "stringExtensions")]
+        [TestCase("STRING extensions", "sTRINGExtensions")]
+        [TestCase("This is a test", "thisIsATest")]
+        [TestCase("A", "a")]
+        public void TestToCamelCase(string input, string expected)
+        {
+            var actual = input.ToCamelCase();
+            actual.Should().Be(expected);
+        }
+
+        [TestCase("pascalCase", "PascalCase")]
+        [TestCase("innerHTML", "InnerHTML")]
+        [TestCase("String Extensions", "StringExtensions")]
+        [TestCase("STRING extensions", "STRINGExtensions")]
+        [TestCase("This is a test", "ThisIsATest")]
+        [TestCase("a", "A")]
+        public void TestToPascalCase(string input, string expected)
+        {
+            var actual = input.ToPascalCase();
+            actual.Should().Be(expected);
+        }
+
+        [TestCase("abc123", "abc123")]
+        [TestCase("abc-123", "abc123")]
+        [TestCase("alpha+_-=][{};:',.<>/?123`/*-+", "alpha123")]
+        public void TestToAlphaNumeric(string input, string expected)
+        {
+            var actual = input.ToAlphaNumeric();
+            actual.Should().Be(expected);
+        }
     }
 }
--- Extensions.Strings/StringExtensions.cs
+++ Extensions.Strings/StringExtensions.cs
@@ -5,9 +5,53 @@ namespace Extensions.Strings
 {
     public static class StringExtensions
     {
+        /// <summary>
+        /// Removes any non-numeric characters from the string.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
         public static string ToNumeric(this string s)
         {
             return new string(s.Where(char.IsDigit).ToArray());
+        }
+
+        /// <summary>
+        /// Converts the string to camelCase.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string ToCamelCase(this string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                throw new ArgumentException("String cannot be empty", nameof(s));
+            }
+
+            var words = s.Split(" ").Select(x => char.ToUpperInvariant(x[0]) + (x.Length > 1 ? x.Substring(1) : string.Empty));
+            s = string.Join(string.Empty, words);
+
+            return char.ToLowerInvariant(s[0]) + (s.Length > 1 ? s.Substring(1) : string.Empty);
+        }
+
+        /// <summary>
+        /// Converts the string to PascalCase.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string ToPascalCase(this string s)
+        {
+            s = s.ToCamelCase();
+            return char.ToUpperInvariant(s[0]) + (s.Length > 1 ? s.Substring(1) : string.Empty);
+        }
+
+        /// <summary>
+        /// Removes any non alpha-numeric characters.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string ToAlphaNumeric(this string s)
+        {
+            return new string(s.Where(char.IsLetterOrDigit).ToArray());
         }
     }
 }
