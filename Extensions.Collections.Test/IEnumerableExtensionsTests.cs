97a64232a05198748cfc9a7f06cc4e3d014cc476
Nathan Dykstra <NathanDykstra@users.noreply.github.com>
2020-08-14 20:22:12 -0500

    Adding BatchBy extension and unit test

---
+++ Extensions.Collections.Test/IEnumerableExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Extensions.Collections.Test
+{
+    [TestFixture]
+    public class IEnumerableExtensionsTests
+    {
+        [TestCase(100, 10, 10)]
+        [TestCase(6000, 10, 600)]
+        [TestCase(10000000, 10000, 1000)]
+        [TestCase(1234567890, 1, 1234567890)]
+        [TestCase(65, 10, 7)]
+        public void TestBatchBy(int listCount, int batchSize, int expectedBatches)
+        {
+            var testList = Enumerable.Range(0, listCount);
+            var results = testList.BatchBy(batchSize);
+
+            foreach (var result in results)
+            {
+                result.Count().Should().BeLessOrEqualTo(batchSize);
+            }
+
+            results.Count().Should().Be(expectedBatches);
+        }
+    }
+}
---
+++ Extensions.Collections/IEnumerableExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Extensions.Collections
+{
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Separates a collection into batches by the specified size or smaller if there are not enough elements.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="batchSize"></param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<T>> BatchBy<T>(this IEnumerable<T> source, int batchSize)
+        {
+            var pos = 0;
+            while (source.Skip(pos).Any())
+            {
+                yield return source.Skip(pos).Take(batchSize);
+                pos += batchSize;
+            }
+        }
+    }
+}
